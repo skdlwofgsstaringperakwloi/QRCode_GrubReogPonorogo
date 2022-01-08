202a5b2a786532e54291b2dc7c1299a5201d5451
unknown <burhanandikaa@gmail.com>
2022-01-08 19:17:31 +0700

    sudah bisa scan QR Code

--- app/src/main/java/bahrudin/bagus/qrcode_grubreogponorogo/MainActivity.kt
+++ app/src/main/java/bahrudin/bagus/qrcode_grubreogponorogo/MainActivity.kt
@@ -1,11 +1,89 @@
 package bahrudin.bagus.qrcode_grubreogponorogo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.budiyev.android.codescanner.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val RC_PERMISSION = 10
+    private var permissionGranted = false
+    private lateinit var codeScanner: CodeScanner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
+
+        codeScanner = CodeScanner(this, scannerView)
+
+        // Parameters (default values)
+        codeScanner.camera = CodeScanner.CAMERA_BACK // or CAMERA_FRONT or specific camera id
+        codeScanner.formats = CodeScanner.ALL_FORMATS // list of type BarcodeFormat,
+        // ex. listOf(BarcodeFormat.QR_CODE)
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE // or CONTINUOUS
+        codeScanner.scanMode = ScanMode.SINGLE // or CONTINUOUS or PREVIEW
+        codeScanner.isAutoFocusEnabled = true // Whether to enable auto focus or not
+        codeScanner.isFlashEnabled = false // Whether to enable flash or not
+
+        // Callbacks
+        codeScanner.decodeCallback = DecodeCallback {
+            runOnUiThread {
+                Toast.makeText(this, "Scan result: ${it.text}", Toast.LENGTH_LONG).show()
+            }
+        }
+        codeScanner.errorCallback = ErrorCallback { // or ErrorCallback.SUPPRESS
+            runOnUiThread {
+                Toast.makeText(this, "Camera initialization error: ${it.message}",
+                    Toast.LENGTH_LONG).show()
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    permissionGranted = false;
+                    requestPermissions(arrayOf(Manifest.permission.CAMERA), RC_PERMISSION);
+                } else {
+                    permissionGranted = true;
+                }
+            } else {
+                permissionGranted = true;
+            }
+        }
+
+        scannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+
+    // permission
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == RC_PERMISSION) {
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                permissionGranted = true
+                codeScanner.startPreview()
+            } else {
+                permissionGranted = false
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (permissionGranted) {
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
     }
 }
